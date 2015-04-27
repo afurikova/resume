ffaff8d5303af9cb15ed420846f211b708d524ed
afurikova <afurikova@yahoo.fr>
2015-04-27 13:27:43 +0200

    make sure the map bounds get updated on page resize

--- js/helper.js
+++ js/helper.js
@@ -200,16 +200,11 @@ function initializeMap() {
 
 }
 
-/*
-Uncomment the code below when you're ready to implement a Google Map!
-*/
-
-// Calls the initializeMap() function when the page loads
+// call the initializeMap() function when the page loads
 window.addEventListener('load', initializeMap);
 
-// Vanilla JS way to listen for resizing of the window
-// and adjust map bounds
-// window.addEventListener('resize', function(e) {
-//   // Make sure the map bounds get updated on page resize
-//   map.fitBounds(mapBounds);
-// });
+// listen for resizing of the window and adjust map bounds
+window.addEventListener('resize', function(e) {
+  // make sure the map bounds get updated on page resize
+  map.fitBounds(mapBounds);
+});
